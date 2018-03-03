ea7867fb23a0be6b7bb4575319f8bafcff4773dd
Maksim Kazak <kazakmaksims@gmail.com>
2018-03-04 00:11:43 +0300

    Preparation

---
+++ src/components/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        msg: 12
+      };
+    }
+  }
+</script>
---
+++ src/router.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Main from './components/Main.vue'
+
+Vue.use(VueRouter);
+
+let routes = [
+  {
+    path: '/',
+    component: Main
+  }
+];
+
+let router = new VueRouter({
+  routes,
+  mode: 'history',
+  linkActiveClass: 'is-open',
+  linkExactActiveClass: 'is-active'
+});
+
+export default router;
